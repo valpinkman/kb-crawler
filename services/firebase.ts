084e6633006ad5fd2b012edbc70653ee0b4a8305
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-03-08 16:50:01 +0100

    test firebase key

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,12 +1,12 @@
 import { config } from "dotenv";
 config();
 import admin from "firebase-admin";
-const { privateKey } = JSON.parse(process.env.FB_PRIVATE_KEY || "");
+// const { privateKey } = JSON.parse(process.env.FB_PRIVATE_KEY || "");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FB_PROJECT_ID,
-      privateKey: privateKey,
+      privateKey: process.env.FB_PRIVATE_KEY,
       clientEmail: process.env.FB_CLIENT_EMAIL,
     }),
   });
