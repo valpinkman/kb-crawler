0b23d9810272d5f9d79b7ac9dce07255b8c3f33c
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-03-08 17:28:40 +0100

    env y u be a bitch

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,12 +1,12 @@
 import { config } from "dotenv";
 config();
 import admin from "firebase-admin";
-// const { privateKey } = JSON.parse(process.env.FB_PRIVATE_KEY || "");
+const { privateKey } = JSON.parse(process.env.FB_PRIVATE_KEY || "");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FB_PROJECT_ID,
-      privateKey: process.env.FB_PRIVATE_KEY,
+      privateKey,
       clientEmail: process.env.FB_CLIENT_EMAIL,
     }),
   });
